fb45920511612a52f16dfc93849b4250348d4ba9
phonghao122 <phonghao122@gmail.com>
2026-07-26 01:41:23 +0700

    sprint 4

--- m6-thermal-power-plant/src/services/lubricationPlanService.js
+++ m6-thermal-power-plant/src/services/lubricationPlanService.js
@@ -1,13 +1,9 @@
 import apiClient from "./apiClient";
 
-
 const BASE_URL = "/api/v1/lubrication-plan";
-
 
 const lubricationPlanService = {
 
-
-    // lấy danh sách có phân trang
     search: async (
         keyword = "",
         status = "",
@@ -30,8 +26,6 @@ const lubricationPlanService = {
         return response.data;
     },
 
-
-    // thêm mới kế hoạch bôi trơn
     create: async (data) => {
 
         const response = await apiClient.post(
@@ -42,12 +36,7 @@ const lubricationPlanService = {
         return response.data;
     },
 
-
-    // cập nhật (sau này dùng)
-    update: async (
-        id,
-        data
-    ) => {
+    update: async (id, data) => {
 
         const response = await apiClient.put(
             `${BASE_URL}/${id}`,
@@ -57,11 +46,7 @@ const lubricationPlanService = {
         return response.data;
     },
 
-
-    // lấy chi tiết
-    getById: async (
-        id
-    ) => {
+    getById: async (id) => {
 
         const response = await apiClient.get(
             `${BASE_URL}/${id}`
@@ -70,18 +55,45 @@ const lubricationPlanService = {
         return response.data;
     },
 
-
-    // xóa
-    // xóa
     remove: async (id) => {
+
         const response = await apiClient.delete(
             `${BASE_URL}/delete/${id}`
+        );
+
+        return response.data;
+    },
+
+    checklist: async (
+        systemId = null,
+        status = null,
+        page = 0,
+        size = 10
+    ) => {
+
+        const params = {
+            page,
+            size
+        };
+
+        if (systemId) {
+            params.systemId = systemId;
+        }
+
+        if (status) {
+            params.status = status;
+        }
+
+        const response = await apiClient.get(
+            `${BASE_URL}/checklist`,
+            {
+                params
+            }
         );
 
         return response.data;
     }
 
 };
-
 
 export default lubricationPlanService;
